26c7f98d54c3e6eaf7f91fef71863eb896384edc
Akshay Anand <aks9800@gmail.com>
2017-06-03 23:10:46 -0400

    delete demos

--- demos/demo1/src/main.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-window.v = new Vue({
-  el: '#app',
-  render: h => h(App)
-});
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import GoodTable from './components/Table.vue';
 
 const GoodTablePlugin = {
-  install(Vue, options) {
+  install: function(Vue, options) {
     Vue.component(GoodTable.name, GoodTable);
   },
 };
