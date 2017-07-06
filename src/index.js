3e4ed91d2c8b83a8c3faebf6b0ce0eb2a7d2ead3
Akshay Anand <aks9800@gmail.com>
2017-07-06 14:36:30 -0400

    sort/filter fix

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ const GoodTablePlugin = {
 };
 
 export default GoodTablePlugin;
+export {
+   GoodTable as VueGoodTable
+};
 
 // Automatic installation if Vue has been added to the global scope.
 if (typeof window !== 'undefined' && window.Vue) {
