aaec46c9f9a87f08271c33d6e65c9c177e56af25
Bart Ledoux <ledouxb@gmail.com>
2017-12-21 10:23:42 -0600

    Add some tests for Pagination

--- test/unit/specs/Pagination.spec.js
+++ test/unit/specs/Pagination.spec.js
@@ -1,14 +1,48 @@
+import sinon from 'sinon'
 import VueGoodPagination from '@/components/Pagination'
 import { mount } from 'vue-test-utils'
 
 describe('VueGoodPagination', () => {
-  it('should render correct contents', async () => {
-    const wrapper = mount(VueGoodPagination, {propsData: {
+  let wrapper, vm
+  beforeEach(() => {
+    wrapper = mount(VueGoodPagination, {propsData: {
       perPage: 10,
       total: 30}
     })
-    await wrapper.vm.$nextTick()
-    let navBtn = wrapper.element.querySelectorAll('.page-btn')
-    navBtn.length.should.equal(2)
+    vm = wrapper.vm
+  })
+  it('should render 2 page buttons', async () => {
+    await vm.$nextTick()
+    wrapper.findAll('.page-btn').length.should.equal(2)
+    wrapper.contains('select').should.be.true
+  })
+
+  it('should call previousPage when the first button is clicked', async () => {
+    await vm.$nextTick()
+    let previousPageStub = sinon.stub(vm, 'previousPage')
+    wrapper.find('.page-btn:first-child').trigger('click')
+    previousPageStub.called.should.be.true
+  })
+
+  it('should call nextPage when the last button is clicked', async () => {
+    await vm.$nextTick()
+    let nextPageStub = sinon.stub(vm, 'nextPage')
+    wrapper.find('.page-btn:last-child').trigger('click')
+    nextPageStub.called.should.be.true
+  })
+
+  it('should call pageChanged during nextPage', async () => {
+    await vm.$nextTick()
+    let pageChangedStub = sinon.stub(vm, 'pageChanged')
+    vm.nextPage()
+    pageChangedStub.called.should.be.true
+  })
+
+  it('should call pageChanged during previousPage', async () => {
+    await vm.$nextTick()
+    vm.nextPage()
+    let pageChangedStub = sinon.stub(vm, 'pageChanged')
+    vm.previousPage()
+    pageChangedStub.called.should.be.true
   })
 })
