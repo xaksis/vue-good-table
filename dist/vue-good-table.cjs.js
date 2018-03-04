ef95d73debcddb899f24145690a7e1e6dd708b85
Akshay Anand <aks9800@gmail.com>
2018-03-04 11:37:40 -0500

    version upgrade

--- dist/vue-good-table.cjs.js
+++ dist/vue-good-table.cjs.js
@@ -1,5 +1,5 @@
 /**
- * vue-good-table v2.0.0-alpha.3
+ * vue-good-table v2.0.0-alpha.4
  * (c) 2018-present xaksis <shay@crayonbits.com>
  * https://github.com/xaksis/vue-good-table
  * Released under the MIT License.
