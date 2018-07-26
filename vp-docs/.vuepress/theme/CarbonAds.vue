efb66177498b9b2f3a3bcf79f4602e7f1d51b61c
Akshay Anand <aks9800@gmail.com>
2018-07-26 11:04:15 -0400

    ads

---
+++ vp-docs/.vuepress/theme/CarbonAds.vue
@@ -0,0 +1,78 @@
+<template>
+<div class="carbon-ads">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'CarbonAds',
+  props: [],
+  data() {
+    return {
+    };
+  },
+  watch: {
+    '$route' (to, from) {
+      console.log('in-route');
+      if (
+        to.path !== from.path &&
+        this.$el.querySelector('#carbonads')
+      ) {
+        this.$el.innerHTML = '';
+        this.load();
+      }
+    }
+  },
+  computed: {
+  },
+  methods: {
+    load () {
+      const s = document.createElement('script');
+      s.id = '_carbonads_js';
+      s.src = `//cdn.carbonads.com/carbon.js?serve=CK7DC53W&placement=xaksisgithubio`;
+      this.$el.appendChild(s);
+    }
+  },
+  mounted() {
+    this.load();
+  },
+  components: {
+  },
+};
+</script>
+
+<style lang="scss">
+.carbon-ads{
+  min-height: 102px;
+  padding: 1.5rem 1.5rem 0;
+  margin-bottom: -.5rem;
+  font-size: .75rem;
+  a {
+    color: #444;
+    font-weight: normal;
+    display: inline;
+  }
+  .carbon-img {
+    float: left;
+    margin-right: 1rem;
+    border: 1px solid #ddd;
+    img {
+      display: block;
+    }
+  }
+
+  .carbon-poweredby {
+    color: #999;
+    display: block;
+    margin-top: 0.5em;
+  }
+}
+@media (max-width: 720px) {
+  .carbon-ads {
+    .carbon-img img {
+      width: 100px;
+      height: 77px;
+    }
+  }
+}
+</style>
--- vp-docs/.vuepress/theme/Layout.vue
+++ vp-docs/.vuepress/theme/Layout.vue
@@ -1,16 +1,16 @@
 <template>
   <Layout>
-    <Banner slot="sidebar-top"/>
+    <carbon-ads slot="sidebar-top"/>
   </Layout>
 </template>
 
 <script>
 import Layout from '@default-theme/Layout.vue'
-import Banner from './Banner.vue'
+import CarbonAds from './CarbonAds.vue'
 export default {
   components: {
     Layout,
-    Banner
+    'carbon-ads': CarbonAds,
   }
 }
 </script>
