19c69742d0e07720f37d6d02052e3a8c7307ded8
Akshay Anand <aks9800@gmail.com>
2019-08-04 18:02:44 -0400

    bundle regenerated

--- dist/vue-good-table.min.js
+++ dist/vue-good-table.min.js
@@ -1,5 +1,5 @@
 /**
- * vue-good-table v2.17.4
+ * vue-good-table v2.17.5
  * (c) 2018-present xaksis <shay@crayonbits.com>
  * https://github.com/xaksis/vue-good-table
  * Released under the MIT License.
